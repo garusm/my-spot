0747f84967fb06ddfc4daef8a9b14a3cc0ee7c78
TIEfigh1er-pil0t <marcin.garus@gmail.com>
2021-06-30 20:26:28 +0200

    js script /1

---
+++ src/main/resources/static/js/albumsScript.js
@@ -0,0 +1,11 @@
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const mySpot = '/spotify/albums/';
+
+function submitted(event) {
+    event.preventDefault();
+    const url = mySpot + q.value;
+    const win = window.open(url, "_self");
+    win.focus();
+}
+f.addEventListener('submit', submitted);
